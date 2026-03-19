f02c9c03d070c12b6584099a9e29f9ba0a6167e0
Eruviel117 <euruviel.marquez@tecdesoftware.edu.mx>
2026-03-19 11:42:37 -0600

    Actualizacion para la entrega dos jeje

--- ProductoPerecible.java
+++ ProductoPerecible.java
@@ -3,25 +3,46 @@ public class ProductoPerecible extends Producto {
     private double descuento;
     private String categoria;
 
-    public ProductoPerecible(String codigo, String nombre, String fechaVencimiento, String categoria, int cantidad, double precio, double descuento){
+    public ProductoPerecible(String codigo, String nombre, String fechaVencimiento, String categoria, int cantidad, double precio, double descuento) throws ProductoException{
         super(codigo, nombre, cantidad, precio);
+
+        if (fechaVencimiento == null || fechaVencimiento.trim().isEmpty()){
+            throw new ProductoException("La fecha de vencimiento no puede estar vacía ");
+        }
+        if (categoria == null || categoria.trim().isEmpty()){
+            throw new ProductoException("Error la categoría no puede estar vacía ");
+        }
+        if (descuento < 0 ){
+            throw new ProductoException(" Error el descuento tiene que ser entre 0 y 100 %");
+        }
+
+
         this.categoria = categoria;
         this.descuento =descuento;
         this.fechaVencimiento = fechaVencimiento;
 
     }
+
+    //agregar al UML
+    public void setDescuento(double descuento)throws ProductoException {
+        if (descuento < 0 || descuento > 100){
+            throw new ProductoException(" Error el descuento tiene que ser entre 0 y 100");
+        }
+        this.descuento = descuento;
+    }
+
     @Override
-    double calcularPreciofinal(){
-        return precio - (precio * (descuento /100)) ;
+    double calcularPrecioFinal(){
+        return getPrecio() - (getPrecio() * (descuento /100)) ;
     }
 
     @Override
     public String generarEtiqueta(){
-        return "Producto: " + nombre + " codigo: " + codigo + "Descuento de: "+ descuento + "Precio: " + calcularPreciofinal();
+        return "Producto: " + getNombre() + " codigo: " + getNombre() + "Descuento de: "+ descuento + "Precio total: " + calcularPrecioFinal();
     }
     @Override
     public String toString() {
-        return super.toString() + " Vence: " + fechaVencimiento+ " Descuento: " + descuento + " % ";
+        return super.toString() + " Vence: " + fechaVencimiento+ " Descuento: " + descuento + " % " + "Categoria" + categoria;
     }
     
 
