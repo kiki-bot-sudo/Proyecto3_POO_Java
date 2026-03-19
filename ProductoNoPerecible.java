ea8883a8c68d244bf51265812017ea49edfae28c
Eruviel117 <euruviel.marquez@tecdesoftware.edu.mx>
2026-03-19 11:51:06 -0600

    Acualizacion de la clase productoNoPerecible

--- ProductoNoPerecible.java
+++ ProductoNoPerecible.java
@@ -1,24 +1,42 @@
 public class ProductoNoPerecible extends Producto  {
     private String categoria;
-    private double descuento;
+    private double promocion; // agregar al UMl
+    
 
     
-    public ProductoNoPerecible(String codigo, String nombre, String categoria, int cantidad, double precio, double descuento){
+    public ProductoNoPerecible(String codigo, String nombre, String categoria, int cantidad, double precio, double promocion)throws ProductoException{
         super(codigo, nombre, cantidad, precio);
+        if (categoria == null || categoria.trim().isEmpty()){
+            throw new ProductoException("Error la categoría no puede estar vacía ");
+        }
+        if(promocion < 0 || promocion > 100){
+            throw new ProductoException("Error la promocion no puede ser negativo debe de ser entre 0 y 100 %");
+        }
         this.categoria = categoria;
-        this.descuento =descuento;
+        this.promocion = promocion;
+    }
+
+    public void setPromocion(double promocion)throws ProductoException {
+        if(promocion < 0 || promocion > 100){
+            throw new ProductoException("Error la promocion no puede ser negativo debe de ser entre 0 y 100 %");
+        }
+        this.promocion = promocion;
+    }
+    public double getPromocion() {
+        return promocion;
     }
 
     @Override
-    double calcularPreciofinal(){
-        return precio - (precio * (descuento /100)) ;
+    double calcularPrecioFinal(){
+        return getPrecio() - (getPrecio() * (promocion /100)) ;
     }
     @Override
     public String generarEtiqueta(){
-        return "Producto: " + nombre + " codigo: " + codigo  + "Precio: " + calcularPreciofinal();
+        return "Producto: " + getNombre() + " codigo: " + getCodigo()  + "Precio: " + calcularPrecioFinal();
     }
+
     @Override
     public String toString() {
-    return super.toString();
+        return super.toString() + " Promocion: " + promocion +" % " + "Categoria" + categoria;
     }
 }
