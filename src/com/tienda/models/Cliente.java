056923ab8289a05a8d292f2342792377c668f144
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-03-26 08:13:14 -0600

    Corregir clase Cliente

--- src/com/tienda/models/Cliente.java
+++ src/com/tienda/models/Cliente.java
@@ -1,16 +1,35 @@
 package com.tienda.models;
 
 public class Cliente {
+    private String id;
     private String nombre;
+    private String email;
+    
 
-    public Cliente(String nombre) {
-        if (nombre == null || nombre.trim().isEmpty()) {
-            throw new IllegalArgumentException("El nombre del cliente no puede estar vacío");
-        }
+    public Cliente (String id, String nombre, String email) {
+        this.id = id;
         this.nombre = nombre;
+        this.email = email;
+        
     }
-
+    public String getId() {
+        return id;
+    }
     public String getNombre() {
         return nombre;
     }
+    public String getEmail() {
+        return email;
+    }
+   
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
 }
+
