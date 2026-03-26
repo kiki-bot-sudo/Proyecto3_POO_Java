5f9d8d43c2c43aaac0e7099d6f523058ac145f9d
Marcelo-Med <marcelo.medina.canto018@gmail.com>
2026-03-26 12:39:58 -0600

    Eliminar metodos inecesarios de clase empleado

--- src/com/tienda/empleados/Empleado.java
+++ src/com/tienda/empleados/Empleado.java
@@ -25,18 +25,6 @@ public abstract class Empleado {
         this.salario = salario;
         this.puesto = puesto;
     }
-    
- private boolean salarioValido(double salario) {
-        return salario > 0; 
-    }
-
-    public void setSalario(double salario) throws SalarioInvalidoException {
-        if (salarioValido(salario)) {
-            this.salario = salario;
-        } else {
-            throw new SalarioInvalidoException(salario);
-        }
-    }
 
     // Getters
     public String getId() {
