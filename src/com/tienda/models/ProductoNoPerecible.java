30378bd4418b37a73efef03792c7796c8bb01486
Eruviel117 <euruviel.marquez@tecdesoftware.edu.mx>
2026-04-15 18:31:13 -0600

    Reorganización de codigo

--- src/com/tienda/models/ProductoNoPerecible.java
+++ src/com/tienda/models/ProductoNoPerecible.java
@@ -1,12 +1,10 @@
 package com.tienda.models;
-
 import com.tienda.exceptions.ProductoException;
 
-public class ProductoNoPerecible extends Producto  {
+
+public class ProductoNoPerecible extends Producto  { 
     private String categoria;
     private double promocion; 
-    
-
     
     public ProductoNoPerecible(String codigo, String nombre, String categoria, int cantidad, double precio, double promocion)throws ProductoException{
         super(codigo, nombre, cantidad, precio);
@@ -20,7 +18,8 @@ public class ProductoNoPerecible extends Producto  {
         this.promocion = promocion;
     }
 
-     @Override
+    // metodos 
+    @Override
     public boolean estaDisponible() {
         return getCantidad() > 0;
     }
@@ -36,31 +35,20 @@ public class ProductoNoPerecible extends Producto  {
     }
 
     @Override
-    public String toCSV() {
-        return "No Perecible," + super.toCSV() + "," + categoria + "," + promocion;                
-    }
-
-    public void setPromocion(double promocion)throws ProductoException {
-        if(promocion < 0 || promocion > 100){
-            throw new ProductoException("Error la promocion no puede ser negativo debe de ser entre 0 y 100 %");
-        }
-        this.promocion = promocion;
-    }
-    public double getPromocion() {
-        return promocion;
-    }
-    public String getCategoria() {
-        return categoria;
-    }
-
-    @Override
     public String generarEtiqueta(){
         return "Producto: " + getNombre() + " codigo: " + getCodigo()  + "Precio: " + calcularPrecioFinal();
     }
 
+    //persistencia
+
+    @Override
+    public String toCSV() {
+        return "No_Perecible" + super.toCSV() + "," + categoria + "," + promocion;                
+    }
+    
     @Override
     public String toString() {
-        return super.toString() + " Promocion: " + promocion +" % " + "Categoria" + categoria;
+        return super.toString() + "Promocion: " + promocion +" % " + "Categoria" + categoria;
     }
 
     public static ProductoNoPerecible fromCSV(String linea) throws ProductoException  {
@@ -68,9 +56,7 @@ public class ProductoNoPerecible extends Producto  {
 
         if (p.length < 7) {
             throw new ProductoException("Línea inválida para ProductoNoPerecible (se esperan o se necesitan 7 campos): " + linea);
-        }
-
-    
+        } 
         double precio;
         int    cantidad;
         double descuento;
@@ -81,12 +67,25 @@ public class ProductoNoPerecible extends Producto  {
         } catch (NumberFormatException e) {
             throw new ProductoException("Datos numéricos inválidos en la linea: " + linea);
         }
-        return new ProductoNoPerecible(
-            p[1].trim(), p[2].trim(), p[5].trim(), 
-            cantidad,precio,descuento      
-        );
+        return new ProductoNoPerecible(p[1].trim(), p[2].trim(), p[5].trim(), cantidad,precio,descuento);
     }
     
+    //getter
+
+    public double getPromocion() {
+        return promocion;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+
+    //setter
+    public void setPromocion(double promocion)throws ProductoException {
+        if(promocion < 0 || promocion > 100){
+            throw new ProductoException("Error la promocion no puede ser negativo debe de ser entre 0 y 100 %");
+        }
+        this.promocion = promocion;
+    }
 
 
 }
