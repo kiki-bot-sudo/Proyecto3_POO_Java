5232b62b87447e883b0b72b7977c2c53f905f34a
Eruviel117 <euruviel.marquez@tecdesoftware.edu.mx>
2026-04-15 18:44:34 -0600

    Reorganización de codigo clase Perecible

--- src/com/tienda/models/ProductoPerecible.java
+++ src/com/tienda/models/ProductoPerecible.java
@@ -1,9 +1,8 @@
 package com.tienda.models;
-
 import com.tienda.exceptions.ProductoException;
 
-public class ProductoPerecible extends Producto {
-    private String fechaVencimiento;
+public class ProductoPerecible extends Producto { 
+    private String fechaVencimiento;   
     private double descuento;
     private String categoria;
 
@@ -16,24 +15,15 @@ public class ProductoPerecible extends Producto {
         if (categoria == null || categoria.trim().isEmpty()){
             throw new ProductoException("Error la categoría no puede estar vacía ");
         }
-        if (descuento < 0 ){
+        if (descuento < 0 || descuento > 100 ){
             throw new ProductoException(" Error el descuento tiene que ser entre 0 y 100 %");
         }
-
         this.categoria = categoria;
         this.descuento =descuento;
         this.fechaVencimiento = fechaVencimiento;
-
     }
-
-    //agregar al UML
-    public void setDescuento(double descuento)throws ProductoException {
-        if (descuento < 0 || descuento > 100){
-            throw new ProductoException(" Error el descuento tiene que ser entre 0 y 100");
-        }
-        this.descuento = descuento;
-    }
-
+    
+    // Metodos 
     // agregar 
     @Override
     public boolean estaDisponible() {
@@ -47,45 +37,30 @@ public class ProductoPerecible extends Producto {
     public double calcularPrecioFinal() {
         return estrategiaPrecio.calcular(getPrecio(), descuento, getCantidad());
     }
-    
 
     @Override
     public String generarEtiqueta(){
         return "Producto: " + getNombre() + " codigo: " + getCodigo() + "Descuento de: "+ descuento + "Precio total: " + calcularPrecioFinal();
     }
+
+
+
+    //persistencia 
     @Override
     public String toString() {
         return super.toString() + " Vence: " + fechaVencimiento+ " Descuento: " + descuento + " % " + "Categoria" + categoria;
     }
 
-    public String getFechaVencimiento() {
-        return fechaVencimiento;
-    }
-    public double getDescuento() {
-        return descuento;
-    }
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setFechaVencimiento(String fechaVencimiento) {
-        this.fechaVencimiento = fechaVencimiento;
-    }
     @Override
     public String toCSV() {
-        return "PERECIBLE," + super.toCSV()
-            
-            + "," + fechaVencimiento + "," + categoria + "," + descuento;                    
+        return "Perecible," + super.toCSV()+ "," + fechaVencimiento + "," + categoria + "," + descuento;                    
     }
 
     public static ProductoPerecible fromCSV(String linea) throws ProductoException  {
         String[] p = linea.split(",");
-
         if (p.length < 8) {
             throw new ProductoException("Linea inválida para ProductoPerecible (se esperan o se necesitan 8 campos): " + linea);
         }
-
-
         double precio;
         int    cantidad;
         double descuento;
@@ -96,18 +71,32 @@ public class ProductoPerecible extends Producto {
         } catch (NumberFormatException e) {
             throw new ProductoException("Datos numéricos inválidos en la linea: " + linea);
         }
-
-        return new ProductoPerecible(
-            p[1].trim(),p[2].trim(),p[5].trim(),p[6].trim(),   
-            cantidad,precio, descuento      
-        );
+        return new ProductoPerecible(p[1].trim(),p[2].trim(),p[5].trim(),p[6].trim(),cantidad,precio, descuento );
     }
-    
 
+    //getter 
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+    public double getDescuento() {
+        return descuento;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
 
-    
+    // setters
+    //agregar al UML
+    public void setDescuento(double descuento)throws ProductoException {
+        if (descuento < 0 || descuento > 100){
+            throw new ProductoException(" Error el descuento tiene que ser entre 0 y 100");
+        }
+        this.descuento = descuento;
+    }
 
-
-
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
 }
+
 
